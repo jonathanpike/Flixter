8cf71c9a9c0a7e666e97f45e05969b2418524814
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2015-10-15 10:54:38 +0000

    Added content to index, added careers and team pages, styled everything

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,12 @@
 class StaticPagesController < ApplicationController
   def index
+    @courses = Course.all
+  end
+
+  def team
+  end
+
+  def careers
   end
 
   def privacy
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :courses
+  has_many :courses, dependent: :destroy
   has_many :enrollments, dependent: :destroy
   has_many :enrolled_courses, :through => :enrollments, :source => :course
 
